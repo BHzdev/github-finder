7bdb404eee989e797a92707ac2a113608c1aed8b
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-08-05 14:23:35 -0300

    feat: add error and loader components

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 import Search from "../components/Search";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
 import { UserProps } from "../types/User";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadUser = async function (userName: string) {
     setUser(null);
+    setIsLoading(true);
 
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    setIsLoading(false);
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -28,7 +42,9 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
+      {isLoading && <Loader />}
       {user && <p>{user.login}</p>}
+      {error && <Error />}
     </div>
   );
 };
