44ec2b8df2f0bb1b0b711fa6272f9466d3c40a1f
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-08-05 13:55:20 -0300

    feat: creating components and types for user search

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import Search from "../components/Search";
+import { UserProps } from "../types/User";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = res.json();
+    console.log(data);
+  };
+  return (
+    <div>
+      <Search loadUser={loadUser} />
+    </div>
+  );
 };
 
 export default Home;
